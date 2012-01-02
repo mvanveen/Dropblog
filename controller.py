4c6bdf886116a8580b1adaa3a086d5ef294d29ab
Michael Van Veen <mvanveen@getaround.com>
2012-01-01 16:39:50 -0800

    stable .1 release

--- controller.py
+++ controller.py
@@ -1,42 +1,93 @@
 import simplejson as json
 import logging
 
-from bottle import route, run
+from bottle import route, run, request
+from google.appengine.api import memcache
 import markdown
 
 from models import (
+  get_header,
+  get_index,
+  get_footer,
   get_post_front_matter,
   get_post,
   get_posts,
+  get_posts_html,
   get_posts_time,
   get_post_no,
   split_file
 )
 
 
-@route('/posts')
-def route_posts():
-  '''Returns a JSON dictionary about current posts'''
-  posts = get_posts_time()
-  logging.error(posts)
-  posts = [get_post_front_matter(post.get('path'), overwrite=True) for post in posts]
+@route('/')
+@route('/index')
+def get_root():
+  overwrite = request.query.overwrite or False
+  return ''.join((
+    get_header(overwrite=overwrite),
+    get_index(overwrite=overwrite),
+    get_footer(overwrite=overwrite)
+  ))
 
-  return {
-    'posts': [{
-        'time': x.get('time'),
-        'last_modified': x.get('time'),
-        'title': x.get('title')
-      } for x in posts
-    ]}
+
+@route('/posts')
+def route_posts(fun=get_posts_html, **kw):
+  '''Returns a JSON dictionary about current posts'''
+
+  posts = get_posts_time()
+  overwrite = request.query.overwrite or False
+  kw['overwrite'] = overwrite
+
+  logging.info('overwrite: %s' % overwrite)
+
+  posts = [
+    get_post_front_matter(
+      post.get('path'),
+      overwrite=overwrite
+    ) for post in posts
+  ]
+  
+  req_type = request.query.type
+  if req_type == 'json':
+  
+    return {
+      'posts': [{
+          'id': id_val,
+          'time': x.get('time'),
+          'last_modified': x.get('time'),
+          'title': x.get('title')
+        } for id_val, x in enumerate(posts)
+      ]}
+
+  else:
+    return (
+      ''.join((
+      get_header(**kw),
+      fun(**kw),
+      '<ul>%s</ul>' % '\n'.join(
+      ["<li><a href='/posts/%s'>%s</a></li>" % 
+        (pid, x.get('title')) for pid, x in enumerate(posts)
+      ]),
+      get_footer(**kw)
+    )))
  
 
-@route('/posts/md/:post_id')
+@route('/posts/:post_id')
 def route_post_md(post_id=None):
-  return markdown.markdown(split_file(get_post_no(post_id))[1])
+  req_type = request.query.type
 
+  if req_type == 'json':
+    return {'post': get_post_no(post_id)}
+  else:
+    template  = memcache.get('')
+    overwrite = request.query.overwrite or False
 
-@route('/posts/plain/:post_id')
-def route_post(post_id=None):
-  return {'post': get_post_no(post_id)}
+    return ''.join((
+      get_header(overwrite=overwrite),
+      markdown.markdown(
+        split_file(get_post_no(post_id, overwrite=overwrite))[1]
+      ),
+      get_footer(overwrite=overwrite)
+    ))
 
 run(server='gae')
--- models.py
+++ models.py
@@ -8,17 +8,26 @@ import yaml
 from dropbox_client import get_client
 
 
+# Dropbox API endpoints
 path_url = 'https://api-content.dropbox.com/1/files/sandbox' # don't add a `/`!
 metadata_url = 'https://api.dropbox.com/1/metadata/sandbox/'
 
+# Get a dropbox client (pickled)
 client = get_client()
+
+# helper to download a RESTFUL endpoint with the GET method.
 get = lambda x: client.request(x, 'GET')[1]
 
+# A sorting function for posts. Sorts by filename.
 path_fun = lambda x: x['path']
 
 
+def get_posts(sort_key=path_fun):
+  '''Returns posts as a sorted dictionary.
 
-def get_posts(sort_key=path_fun, overwrite=False):
+      TODO: add caching
+
+  '''
   url = ''.join((metadata_url, 'posts'))
   logging.info('url is: %s' % url)
 
@@ -29,39 +38,67 @@ def get_posts(sort_key=path_fun, overwrite=False):
 
 
 def get_post_front_matter(file_str, overwrite=False):
-  file_str = split_file(get_post(file_str, overwrite=overwrite))
-  logging.info(file_str)
+  '''Helper function which turns the front-matter of a post into YAML
+
+    *e.g.*
+
+      Title: This is a title         title: This is a title
+      Author: This is an author  =>  author: This is an author
+      Time: 123456789                time: 123456789
+
+      My great post starts here              ||
+                                            \\//
+
+                                      {'title': 'This is a title',
+                                        'author': 'This is an author',
+                                        'time': 123456789
+                                      }
+  '''
+  file_str = split_file(
+    get_post(
+      file_str,
+      overwrite=overwrite
+    ))
+
   return dict([
-    (key.lower(), val) for key, val in yaml.load(file_str[0]).iteritems()
+    (key.lower(), val) for key, val 
+      in yaml.load(file_str[0]).iteritems()
   ])
 
 
 def sort_time(obj):
+  '''Used as a sorting function which returns a post's time: header vlaue
+
+  Side effect: Reads a post to determine its date.
+
+  '''
   obj = obj.get('path')
   result = split_file(obj)
 
   frontmatter = get_post_front_matter(result[0])
 
-  found_time = frontmatter['title']
+  found_time = frontmatter['time']
   
   logging.info('time: %s' % found_time)
-  return found_time
+  return int(found_time)
 
 
 def get_posts_time(**kw):
+  '''Sort posts by time header'''
+
   kw.update({'sort_key': sort_time})
   return get_posts(**kw)
 
 
-def get_post_no(no):
+def get_post_no(no, **kw):
   '''Gets post based off of index number'''
 
   post = get_posts_time()[int(no)]['path']
   logging.info('getting post: %s' % post)
-  return get_post(post)
+  return get_post(post, **kw)
 
 
-def get_post(path, overwrite=False):
+def get_item(path, overwrite=False):
   assert isinstance(path, basestring), 'Expected path to be a string'
 
   prefix = 'file_'
@@ -77,7 +114,6 @@ def get_post(path, overwrite=False):
     while not result: 
       result = memcache.set(key, fileObj)
   
-  logging.info('fileObj: %s' % fileObj)
   return fileObj
 
 
@@ -85,3 +121,23 @@ def split_file(file_str):
   result = re.split(r'\n\n', file_str)
   return (result[0], '\n\n'.join(result[1:]))
 
+
+def get_post(path, **kw):
+  return get_item(path, **kw)
+
+
+def get_header(**kw):
+  header = get_item('/templates/header.html', **kw)
+  return header
+
+
+def get_footer(**kw):
+  return get_item('/templates/footer.html', **kw)
+
+
+def get_posts_html(**kw):
+  return get_item('/templates/posts.html', **kw)
+
+
+def get_index(**kw):
+  return get_item('/templates/index.html', **kw)
